adcbcdfbb3a0c4bae002fe5e504872f9beabf8d0
Vincent Milum Jr <git@darkain.com>
2018-08-27 20:42:34 -0700

    More appropriate buffer size calculation

--- arduino.cpp
+++ arduino.cpp
@@ -28,7 +28,7 @@ static lua_flash_string *lua_flash_first = nullptr;
 ////////////////////////////////////////////////////////////////////////////////
 LUA_API const char *lua_flash_allocate(const char *string) {
 	auto len			= strlen_P(string);
-	auto item			= (lua_flash_string*) malloc(len + sizeof(void*) + 1);
+	auto item			= (lua_flash_string*) malloc(len + sizeof(lua_flash_string) + 1);
 	item->next			= nullptr;
 	memcpy_P(item->buffer, string, len);
 	item->buffer[len]	= '\0';
